5a59c545319177885aa6fb682d4a7870e0f7f595
Nicolas <nicolasgabriel.developer@gmail.com>
2022-12-06 21:56:03 -0300

    Fazendo funcionar a estilização do dark mode

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -7,15 +7,27 @@ const link = document.getElementById('dark-mode')
 //Variavel que define qual link jogar no css
 let themeWhite = true;
 
+//Recebendo elementos que vão mudar a cor na tela
+const lupa = document.getElementById('lupa')
+const arrow = document.getElementById('arrow')
+const moon = document.getElementById('moon')
+
 //Função que muda o tema
 buttonDarkMode.onclick = function (){
 if(themeWhite == true){
     themeWhite = false
     link.href = "assets/css/themetwo.css"
+    lupa.src = "assets/images/lupa_dark.jpg"
+    arrow.src = "assets/images/arrow_up_dark.png"
+    moon.src = "assets/images/moon_dark.svg"
+    
 }
 else if(themeWhite == false ){
     themeWhite = true
     link.href = "assets/css/themeone.css"
+    lupa.src = "assets/images/lupa.jpg"
+    arrow.src = "assets/images/arrow_up.png"
+    moon.src = "assets/images/moon.svg"
 }
 }
 
