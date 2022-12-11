efd7b930ec09f0239a214b92a3dc90dc5e167b3f
Nicolas <nicolasgabriel.developer@gmail.com>
2022-12-11 16:11:17 -0300

    Puxando dados da API e começando a estruturar as funções

--- assets/js/individual.js
+++ assets/js/individual.js
@@ -1,4 +1,41 @@
+let sectionOne = document.getElementById("sectionOne")
+
+function fecharSectionOne(){
+    sectionOne.style.display = "none"
+}
+
+
+
+
+
+
+
+
+let urlData;
+
 function mostrarDados(dado){
+    fecharSectionOne()
     mostrarNada()
-    console.log(dado)
+    
+    dado = dado.toLowerCase()
+    urlData = "https://restcountries.com/v3.1/alpha?codes=" +dado
+
+    console.log(urlData)
+    puxarApi()
+
+}
+
+function puxarApi(){
+    fetch(urlData)
+    .then((response) => {
+        return response.json();
+    })
+    .then((dados) => {
+        console.log(dados[0]);
+
+
+    })
+    .catch((erro) => {
+        console.log("Erro: " + erro);
+    })
 }
