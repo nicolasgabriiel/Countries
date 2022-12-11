57830bda96e944adfd711848c11403844c754cf6
Nicolas <nicolasgabriel.developer@gmail.com>
2022-12-11 17:01:42 -0300

    Começando a exibir dados

--- assets/js/individual.js
+++ assets/js/individual.js
@@ -4,9 +4,9 @@ function fecharSectionOne(){
     sectionOne.style.display = "none"
 }
 
+const informacoesIndividuais = document.getElementById('informacoesIndividuais')
 
-
-
+console.log(informacoesIndividuais)
 
 
 
@@ -31,11 +31,38 @@ function puxarApi(){
         return response.json();
     })
     .then((dados) => {
-        console.log(dados[0]);
+        console.log(dados[0].name.nativeName);
 
+       let dadosDaApi  = `
+       <div class:"button-back" id="back"><button>Back</button></div>
+       <div class="imagem-individual" style="background-image: url(${dados[0].flags.png});"></div>
+       <div class="informacoes">
+       <h2>${dados[0].name}</h2>
+       <p><strong>Native Name:</strong>${dados[0].name.nativeName}</p>
+       <p><strong>Top Level Domain:</strong>${dados[0].tld[0]}</p>
+       <p><strong>Population:</strong>${dados[0].population}</p>
+       <p><strong>Currencies:</strong>${dados[0].currencies.name}</p>
+       <p><strong>Region:</strong>${dados[0].region}</p>
+       <p><strong>Languages:</strong>${dados[0].languages}</p>
+       <p><strong>Sub Region:</strong>${dados[0].subregion}</p>
+       <p><strong>Capital:</strong>${dados[0].capital}</p>
+       </div>
+       <div class="border-countries">
+       <h3>Border Countries</h3>
+       <p>${dados[0]}</p>
+       <p>${dados[0]}</p>
+       <p>${dados[0]}</p>
+       </div>
+        
+        
+        
+        `
 
+        informacoesIndividuais.innerHTML = dadosDaApi
     })
     .catch((erro) => {
         console.log("Erro: " + erro);
     })
+
+    
 }
